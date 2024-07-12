6c826f6808e2129bd64d9ac09aec74f9063f3a2e
takeover-da <dlslyn@naver.com>
2024-07-12 17:12:30 +0900

    7.12 inheritance 수업

--- src/inheritance/Ex3.java
+++ src/inheritance/Ex3.java
@@ -4,7 +4,17 @@ public class Ex3 {
 
 	public static void main(String[] args) {
 
-		//
+		// 일반회원 생성
+		Customer customer = new Customer();
+		customer.customerName = "둘리";
+		customer.calcPrice(10000);
+		customer.showInfo();
+		
+		// VIP회원 생성
+		VIPCustomer vip = new VIPCustomer();
+		vip.customerName = "또치";
+		vip.calcPrice(10000);
+		vip.showInfo();
 		
 	}
 
@@ -23,4 +33,34 @@ class Customer {
 		customerGrade = "SILVER";
 		bonusRatio = 0.01;
 	}
+	
+	// 보너스 포인트 적립하고 지불 가격을 계산하는 메소드
+	public void calcPrice(int price) {
+		
+		bonusPoint = bonusPoint + (price * bonusRatio);
+		System.out.println(customerName + "님이 " + 
+							price + "원을 결제했습니다.");
+	}
+	
+	// 출력 메소드
+	public void showInfo() {
+		System.out.println(customerName + " 님의 등급은 " +
+							customerGrade + "이며, 보너스 포인트는 " +
+							bonusPoint + "입니다.");
+	}
+	
 }
+
+// VIP회원 클래스
+class VIPCustomer extends Customer {  // 상속받기
+	
+	double saleRatio;  // 할인률
+	
+	// 고정값은 생성자에서 초기화
+	public VIPCustomer() {
+		customerGrade = "VIP";
+		bonusPoint = 0.05;
+		saleRatio = 0.1;
+	}
+}
+
---
+++ src/inheritance/Quiz3.java
@@ -0,0 +1,40 @@
+package inheritance;
+
+public class Quiz3 {
+
+	public static void main(String[] args) {
+
+		// 볼펜
+		Bolpen bolpen = new Bolpen();
+		bolpen.volt = 2;
+		bolpen.amountLeft = 100;
+		bolpen.penColor = "blue";
+		
+		// 만년필
+		FountainPen fp = new FountainPen();
+		fp.volt = 3;
+		fp.amountLeft = 80;
+		fp.brand = "MONTBLANC";
+		
+	}
+
+}
+
+// 펜 클래스
+class Pen {
+	
+	int volt;  			// 펜의 굵기
+	double amountLeft;  // 남은 양
+}
+
+// 볼펜 클래스
+class Bolpen extends Pen {
+	
+	String penColor;	// 볼펜의 색
+}
+
+// 만년필 클래스
+class FountainPen extends Pen {
+	
+	String brand;		// 회사명
+}
